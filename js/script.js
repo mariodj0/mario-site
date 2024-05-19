03d663c0908ff3061840d5c322435fa93359c0d4
mariodj0 <112354520+mariodj0@users.noreply.github.com>
2024-05-19 14:42:52 +1000

    chore: Add hamburger menu functionality to improve navigation experience
    - update links for site webmanifest and project
    - add project html with contents

--- js/script.js
+++ js/script.js
@@ -9,3 +9,21 @@ $(document).ready(function() {
         }
     });
 });
+
+
+// Toggle hamburger menu
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
+
+document.addEventListener('click', (event) => {
+    const target = event.target;
+    if (!target.closest('.hamburger') && !target.closest('.nav-links')) {
+        hamburger.classList.remove('active');
+        navLinks.classList.remove('active');
+    }
+});
